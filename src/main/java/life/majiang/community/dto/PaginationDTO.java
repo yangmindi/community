d206ddb55ab8e9f66621698f96e63cb57906288f
yangmindi <1946334058@qq.com>
2019-10-16 14:19:19 +0800

    person information html

--- src/main/java/life/majiang/community/dto/PaginationDTO.java
+++ src/main/java/life/majiang/community/dto/PaginationDTO.java
@@ -17,25 +17,11 @@ public class PaginationDTO {
     private List<Integer> pages = new ArrayList<>();
     private Integer totalPage;
 
-    public void setPagination(Integer totalCount, Integer page, Integer size) {
-        Integer totalPage = 0;
-        if(totalCount % size == 0){
-            totalPage = totalCount / size;
-        }else {
-            totalPage = totalCount / size + 1;
-        }
+    //总页数，当前页数
+    public void setPagination(Integer totalPage, Integer page) {
 
         this.totalPage = totalPage;
-        if(page < 1){
-            page = 1;
-        }
-
-        if(page > totalPage){
-            page = totalPage;
-        }
-
         this.page = page;
-
         pages.add(page);
         for(int i=1; i<=3; i++){
             if(page - i > 0){
