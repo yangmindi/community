12616ad3487ff456934710ea752c13d01f500c91
yangmindi <1946334058@qq.com>
2019-10-17 19:25:02 +0800

    first comment function

---
+++ src/main/java/life/majiang/community/controller/CommentController.java
@@ -0,0 +1,39 @@
+package life.majiang.community.controller;
+
+import life.majiang.community.dto.CommentDTO;
+import life.majiang.community.mapper.CommentMapper;
+import life.majiang.community.model.Comment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+public class CommentController {
+
+    @Autowired
+    private CommentMapper commentMapper;
+
+
+    @ResponseBody
+    @RequestMapping(value = "/comment",method = RequestMethod.POST)
+    public Object post(@RequestBody CommentDTO commentDTO){
+        Comment comment = new Comment();
+        comment.setParentId(commentDTO.getParentId());
+        comment.setContent(commentDTO.getContent());
+        comment.setType(commentDTO.getType());
+        comment.setGmtModified(System.currentTimeMillis());
+        comment.setGmtCreate(System.currentTimeMillis());
+        comment.setCommentator(1);
+        comment.setLikeCount(0L);
+        commentMapper.insert(comment);
+        Map<Object,Object> objectObjectHashMap = new HashMap<>();
+        objectObjectHashMap.put("message","成功");
+        return objectObjectHashMap;
+    }
+}
--- src/main/java/life/majiang/community/interceptor/SessionInterception.java
+++ src/main/java/life/majiang/community/interceptor/SessionInterception.java
@@ -20,9 +20,9 @@ public class SessionInterception implements HandlerInterceptor {
     private UserMapper userMapper;
 
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler){
         Cookie[] cookies = request.getCookies();
-        if(cookies!=null || cookies.length!=0){
+        if(cookies!=null && cookies.length!=0){
             for (Cookie cookie : cookies) {
                 if(cookie.getName().equals("token")){
                     String token = cookie.getValue();
